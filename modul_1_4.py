95dcc513193389abe3b2e8668f9097857d1da149
alekseiM2393 <alexey13345@gmail.com>
2024-09-12 22:56:56 +0300

    Add files via upload

---
+++ modul_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("как дела?" )
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string.replace("good" ," "))
+print(my_string.replace("very" ," "))
