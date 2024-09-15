4483918d992c39c3535191d0f065508faa5ce78d
alekseiM2393 <alexey13345@gmail.com>
2024-09-15 23:23:27 +0300

    Add files via upload

---
+++ modul_1_5.py
@@ -0,0 +1,8 @@
+# immutable_var=3,4,5,False,"hello"
+# print(immutable_var)
+# immutable_var[7]=True
+# print(immutable_var) #кортеж относится к неизменяемым типам данных
+mutable_list=[1,5,"cat"]
+print(mutable_list)
+mutable_list[-1]="dog"
+print(mutable_list)
