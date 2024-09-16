35d607864dbd257dad4caa1dc36c76b11f7b95cd
alekseiM2393 <alexey13345@gmail.com>
2024-09-16 23:45:32 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,17 @@
+# my_dict={"alex":1992, "anna":1988}
+# print(my_dict)
+# print(my_dict["anna"])
+# print(my_dict.get("sasha"))
+# my_dict.update({"sofa":1993, "kira":2003})
+# print(my_dict)
+# a=my_dict.pop("sofa")
+# print(my_dict)
+# print(a)
+
+my_set = {1,2,4,2,1,4, "hello", (8,6,7,), "hello", (8,6,7), True}
+print(my_set)
+my_set.add(9)
+my_set.add(11)
+my_set.discard(2)
+print(my_set)
+
