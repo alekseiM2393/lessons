39ccc134717af9fab1d45d52aa4ea0c0de5be1d4
alekseiM2393 <alexey13345@gmail.com>
2024-10-08 20:37:36 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -14,8 +14,10 @@ def string_info(string):
 def is_contains(string, list_to_search):
     count_calls()
     return string.upper() in [s.upper() for s in list_to_search]
-    print(string_info('Capybara'))
-    print(string_info('Armageddon'))
-    print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
-    print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
-    print(calls)
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
