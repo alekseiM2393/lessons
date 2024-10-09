c8ad2bef7854f55f4a4d832f6c0a30a93fe29f99
alekseiM2393 <alexey13345@gmail.com>
2024-10-09 21:19:32 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,9 +1,9 @@
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    if sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
-    elif '@' not in recipient or not recipient.endswith(
+    if '@' not in recipient or not recipient.endswith(
             ('.com', '.ru', '.net')) or '@' not in sender or not sender.endswith(('.com', '.ru', '.net')):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
     elif recipient == sender:
         print("Нельзя отправить письмо самому себе!")
     else:
@@ -14,3 +14,4 @@ send_email('Это сообщение для проверки связи', 'vasy
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
